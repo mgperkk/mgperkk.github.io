c0845697c35a7cefddfaa023470d34e27231199b
Fedor Rodin <rodinfm307@gmail.com>
2021-05-17 14:25:52 +0300

    16 render with tv

--- 307/16.05/3/data.js
+++ 307/16.05/3/data.js
@@ -54,8 +54,8 @@ var APP_DATA = {
       ],
       "faceSize": 1000,
       "initialViewParameters": {
-        "yaw": 1.2376089489092266,
-        "pitch": 0.09575030308951327,
+        "yaw": 0.1498656981335884,
+        "pitch": 0.14333694598821367,
         "fov": 1.4314062183930185
       },
       "linkHotspots": [
@@ -64,6 +64,24 @@ var APP_DATA = {
           "pitch": 0.2243162393987319,
           "rotation": 0,
           "target": "2-"
+        },
+        {
+          "yaw": 0.2217830289005036,
+          "pitch": 0.3232238047360241,
+          "rotation": 0,
+          "target": "5----"
+        },
+        {
+          "yaw": 0.44803797388273026,
+          "pitch": 0.2991986130285067,
+          "rotation": 0,
+          "target": "6---"
+        },
+        {
+          "yaw": 0.6362876121965098,
+          "pitch": 0.265194198367217,
+          "rotation": 0,
+          "target": "7--"
         }
       ],
       "infoHotspots": []
@@ -178,6 +196,144 @@ var APP_DATA = {
           "pitch": 0.1292165562805394,
           "rotation": 0,
           "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5----",
+      "name": "ТВ: Задачи в облаке",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.5898872685635936,
+        "pitch": 0.19842090816886682,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3600299696500162,
+          "pitch": 0.7047226391646717,
+          "rotation": 0,
+          "target": "6---"
+        },
+        {
+          "yaw": 1.5313897167618666,
+          "pitch": 0.7130415649677619,
+          "rotation": 0,
+          "target": "7--"
+        },
+        {
+          "yaw": 0.7335408000723689,
+          "pitch": 0.17780269165535145,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---",
+      "name": "ТВ: Для абитуриентов",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.5725122106328815,
+        "pitch": 0.19398046068890906,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2269758290254682,
+          "pitch": 0.6769378879772177,
+          "rotation": 0,
+          "target": "5----"
+        },
+        {
+          "yaw": 1.5769962278523275,
+          "pitch": 0.7068310999048801,
+          "rotation": 0,
+          "target": "7--"
+        },
+        {
+          "yaw": 0.7158516999872528,
+          "pitch": 0.16687979637766226,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--",
+      "name": "ТВ: Объявления",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.5763051575840459,
+        "pitch": 0.22918148525684146,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1586489826781907,
+          "pitch": 0.6894639239902833,
+          "rotation": 0,
+          "target": "5----"
+        },
+        {
+          "yaw": 1.3211641800319427,
+          "pitch": 0.7150022531645277,
+          "rotation": 0,
+          "target": "6---"
+        },
+        {
+          "yaw": 0.6859205927824696,
+          "pitch": 0.17441681422393174,
+          "rotation": 0,
+          "target": "1--"
         }
       ],
       "infoHotspots": []
