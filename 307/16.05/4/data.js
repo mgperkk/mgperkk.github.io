6823b365fbc3a104eea185a5b77bf6d2447a4dd4
Fedor Rodin <rodinfm307@gmail.com>
2021-05-17 14:51:56 +0300

    kek

---
+++ 307/16.05/4/data.js
@@ -0,0 +1,153 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.6145657471794443,
+        "pitch": 0.14577668124357857,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024624990970295357,
+          "pitch": 0.20922082291308186,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Граф",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.5087439339647819,
+        "pitch": 0.12069427352873063,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1345717708127303,
+          "pitch": 0.12537919678468512,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Космонавт",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.4879568005257653,
+        "pitch": 0.14235551314389028,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0136189634402335,
+          "pitch": 0.12062068179349339,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.136719628743709,
+          "pitch": 0.12957408473956455,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Столешница",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.4273203417043625,
+        "pitch": 0.11454558485683108,
+        "fov": 1.4314062183930185
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03466844038338124,
+          "pitch": 0.13584912535025495,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Коридор 307 (4 этаж)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
